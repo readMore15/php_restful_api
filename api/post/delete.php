62dfa30c00f8d6ce80dad29a9f7cc7761deae24b
Dan <danemmf@surefirelocal.com>
2018-07-29 20:27:43 +0800

    adds delete api/method

---
+++ api/post/delete.php
@@ -0,0 +1,33 @@
+<?php
+
+// Headers
+header('Access-Control-Allow-Origin: *');
+header('Content-Type: application/json');
+header('Access-Control-Allow-Methods: POST');
+header('Access-Control-Allow-Headers: Access-Control-Allow-Headers, Access-Control-Allow-Methods, Content-Type, Authorization, X-Requested-With');
+
+require_once $_SERVER['DOCUMENT_ROOT'] . '/config/Database.php';
+require_once $_SERVER['DOCUMENT_ROOT'] . '/models/Post.php';
+
+// Instantiate dbase
+$database = new Database();
+$db = $database->connect();
+
+// Instantiate Blog Post object
+$post = new Post($db);
+
+$post->id = isset($_GET['id']) ? $_GET['id'] : die();
+
+// delete post
+if($post->delete()){
+	echo json_encode([
+		'status' 	=> 'success',
+		'message'	=> 'Post delete.'
+	]);
+}
+else{
+	echo json_encode([
+		'status'	=> 'failed',
+		'message'	=> 'Post not deleted.'
+	]);
+}
--- models/Post.php
+++ models/Post.php
@@ -169,4 +169,28 @@ class Post
 
 		return false;
 	}
+
+	// delete blog post
+	public function delete()
+	{
+		$query = 'DELETE FROM
+				' . $this->table . '
+				WHERE
+					id = :id';
+
+		$stmt = $this->conn->prepare($query);
+
+		$this->id = htmlspecialchars(strip_tags($this->id));
+		$stmt->bindParam(':id', $this->id);
+
+		//execute query
+		if($stmt->execute()){
+			return true;
+		}
+
+		// print error if something goes wrong
+		printf("Error: %s.\n", $stmt->error);
+
+		return false;
+	}
 }
